5193873f3d37bedb12df9a1a1ec19ac633285c44
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-28 06:53:08 +0900

    Improve Compress::Raw::Zlib::Deflate.

--- lib/SPVM/Compress/Raw/Zlib/Deflate.c
+++ lib/SPVM/Compress/Raw/Zlib/Deflate.c
@@ -96,3 +96,31 @@ int32_t SPVM__Compress__Raw__Zlib__Deflate__deflateReset(SPVM_ENV* env, SPVM_VAL
   
   return error_id;
 }
+
+int32_t SPVM__Compress__Raw__Zlib__Deflate__deflateTune(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  int good_length = stack[0].ival;
+  int max_lazy = stack[1].ival;
+  int nice_length = stack[2].ival;
+  int max_chain = stack[3].ival;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* obj_z_stream = env->get_field_object_by_name(env, stack, obj_self, "z_stream", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  z_stream* st_z_stream = env->get_pointer(env, stack, obj_z_stream);
+  
+  int32_t status = deflateTune(st_z_stream, good_length, max_lazy, nice_length, max_chain);
+  
+  if (!(status == Z_OK)) {
+    error_id = env->die(env, stack, "[zlib Error]deflateTune() failed(error:%d).", status, __func__, FILE_NAME, __LINE__);
+    goto END_OF_FUNC;
+  }
+  
+  END_OF_FUNC:
+  
+  return error_id;
+}
