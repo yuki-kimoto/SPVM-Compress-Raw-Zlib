367faf3d3d7c98567ec7d92d3afd204d5675050e
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-28 07:15:14 +0900

    Improve Compress::Raw::Zlib::Deflate.

--- lib/SPVM/Compress/Raw/Zlib/Deflate.c
+++ lib/SPVM/Compress/Raw/Zlib/Deflate.c
@@ -144,3 +144,24 @@ int32_t SPVM__Compress__Raw__Zlib__Deflate__total_out(SPVM_ENV* env, SPVM_VALUE*
   
   return error_id;
 }
+
+int32_t SPVM__Compress__Raw__Zlib__Deflate__total_in(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* obj_z_stream = env->get_field_object_by_name(env, stack, obj_self, "z_stream", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  z_stream* st_z_stream = env->get_pointer(env, stack, obj_z_stream);
+  
+  int64_t total_in = st_z_stream->total_in;
+  
+  stack[0].lval = total_in;
+  
+  END_OF_FUNC:
+  
+  return error_id;
+}
+
