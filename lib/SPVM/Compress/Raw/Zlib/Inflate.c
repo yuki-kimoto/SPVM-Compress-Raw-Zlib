9ca02668778003b12f7e9c2aaa75597d291e7da2
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-29 15:28:29 +0900

    Improve Compress::Raw::Zlib::Deflate.

--- lib/SPVM/Compress/Raw/Zlib/Inflate.c
+++ lib/SPVM/Compress/Raw/Zlib/Inflate.c
@@ -62,6 +62,29 @@ int32_t SPVM__Compress__Raw__Zlib__Inflate___inflateInit(SPVM_ENV* env, SPVM_VAL
   return error_id;
 }
 
+int32_t SPVM__Compress__Raw__Zlib__Inflate__inflateReset(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* obj_z_stream = env->get_field_object_by_name(env, stack, obj_self, "z_stream", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  z_stream* st_z_stream = env->get_pointer(env, stack, obj_z_stream);
+  
+  int32_t status = inflateReset(st_z_stream);
+  
+  if (!(status == Z_OK)) {
+    error_id = env->die(env, stack, "[zlib Error]inflateReset() failed(error:%d).", status, __func__, FILE_NAME, __LINE__);
+    goto END_OF_FUNC;
+  }
+  
+  END_OF_FUNC:
+  
+  return error_id;
+}
+
 int32_t SPVM__Compress__Raw__Zlib__Inflate__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
