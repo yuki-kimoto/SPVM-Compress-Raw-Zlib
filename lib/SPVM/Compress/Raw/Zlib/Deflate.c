8eff7e5b9b25187947926f2ca21998ab9a06739e
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-29 13:47:08 +0900

    Improve Compress::Raw::Zlib::Deflate.

--- lib/SPVM/Compress/Raw/Zlib/Deflate.c
+++ lib/SPVM/Compress/Raw/Zlib/Deflate.c
@@ -202,3 +202,104 @@ int32_t SPVM__Compress__Raw__Zlib__Deflate__DESTROY(SPVM_ENV* env, SPVM_VALUE* s
   
   return error_id;
 }
+
+int32_t SPVM__Compress__Raw__Zlib__Deflate__deflate(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* obj_input = stack[1].oval;
+  
+  void* obj_output_ref = stack[2].oval;
+  
+  if (!obj_input) {
+    error_id = env->die(env, stack, "The input $input must be define.", __func__, FILE_NAME, __LINE__);
+    goto END_OF_FUNC;
+  }
+  
+  if (!(obj_output_ref && env->length(env, stack, obj_output_ref) == 1)) {
+    error_id = env->die(env, stack, "The output reference $output_ref must be 1-length string array.", __func__, FILE_NAME, __LINE__);
+    goto END_OF_FUNC;
+  }
+  
+  void* obj_z_stream = env->get_field_object_by_name(env, stack, obj_self, "z_stream", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  z_stream* st_z_stream = env->get_pointer(env, stack, obj_z_stream);
+  
+  char* input = (char*)env->get_chars(env, stack, obj_input);
+  int32_t input_length = env->length(env, stack, obj_input);
+  
+  st_z_stream->next_in = input;
+  st_z_stream->avail_in = input_length;
+  
+  int32_t CRC32 = env->get_field_long_by_name(env, stack, obj_self, "CRC32", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  if (CRC32) {
+    int64_t f_crc32 = env->get_field_long_by_name(env, stack, obj_self, "crc32", &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { goto END_OF_FUNC; }
+    
+    f_crc32 = crc32(f_crc32, st_z_stream->next_in, st_z_stream->avail_in) ;
+    env->set_field_long_by_name(env, stack, obj_self, "crc32", f_crc32, &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { goto END_OF_FUNC; }
+  }
+  
+  int32_t ADLER32 = env->get_field_long_by_name(env, stack, obj_self, "ADLER32", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  if (ADLER32) {
+    int64_t f_adler32 = env->get_field_long_by_name(env, stack, obj_self, "adler32", &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { goto END_OF_FUNC; }
+    
+    f_adler32 = adler32(f_adler32, st_z_stream->next_in, st_z_stream->avail_in) ;
+    env->set_field_long_by_name(env, stack, obj_self, "adler32", f_adler32, &error_id, __func__, FILE_NAME, __LINE__);
+    if (error_id) { goto END_OF_FUNC; }
+  }
+  
+  int64_t Bufsize = env->get_field_long_by_name(env, stack, obj_self, "Bufsize", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { goto END_OF_FUNC; }
+  
+  int32_t output_length = Bufsize;
+  char* output = env->new_memory_block(env, stack, output_length);
+  
+  st_z_stream->next_out = output;
+  st_z_stream->avail_out = Bufsize;
+  
+  while (st_z_stream->avail_in != 0) {
+    
+    if (st_z_stream->avail_out == 0) {
+      int32_t new_output_length = output_length + Bufsize;
+      
+      char* new_output = env->new_memory_block(env, stack, new_output_length);
+      memcpy(new_output, output, output_length);
+      env->free_memory_block(env, stack, output);
+      output = new_output;
+      
+      st_z_stream->next_out = new_output + output_length;
+      st_z_stream->avail_out = Bufsize;
+    }
+    
+    int32_t status = deflate(st_z_stream, Z_NO_FLUSH);
+    
+    if (!(status == Z_OK)) {
+      error_id = env->die(env, stack, "[zlib Error]deflate() failed(error:%d).", status, __func__, FILE_NAME, __LINE__);
+      goto END_OF_FUNC;
+    }
+  }
+  
+  output_length -= st_z_stream->avail_out;
+  
+  void* obj_output = env->new_string(env, stack, output, output_length);
+  
+  env->set_elem_object(env, stack, obj_output_ref, 0, obj_output);
+  
+  END_OF_FUNC:
+  
+  if (output) {
+    env->free_memory_block(env, stack, output);
+  }
+  
+  return error_id;
+}
