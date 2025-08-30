e0a8d6f7ed9bfb677f57f5dc8572a8969588b3aa
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-08-30 09:25:12 +0900

    Add warnings.

--- lib/SPVM/Compress/Raw/Zlib.pm
+++ lib/SPVM/Compress/Raw/Zlib.pm
@@ -15,6 +15,10 @@ SPVM::Compress::Raw::Zlib - Low-Level Interface to zlib compression library
 Compress::Raw::Zlib class in L<SPVM> provides a Perl interface to the I<zlib>
 compression libraries.
 
+B<WARNINGS:>
+
+B<This distribution has a significantly incomplete implementation and hasn't been tested at all, and is completely undocumented.>
+
 =head1 Repository
 
 L<SPVM::Compress::Raw::Zlib - Github|https://github.com/yuki-kimoto/SPVM-Compress-Raw-Zlib>
