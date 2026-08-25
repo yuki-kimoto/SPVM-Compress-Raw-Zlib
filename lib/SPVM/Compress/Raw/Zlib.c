851dcda890751a3bb2e1a0cb8c3c4553839949df
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-08-25 09:08:05 +0900

    Build Compress::Raw::Zlib

---
+++ lib/SPVM/Compress/Raw/Zlib.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2026 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Compress/Raw/Zlib.c";
+
+int32_t SPVM__Compress__Raw__Zlib__dummy(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
